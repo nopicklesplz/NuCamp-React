76098d91382326f2fdebdb666d926c8747a9886a
Jesus Moreno <moreno_2318@yahoo.com>
2023-03-26 20:41:40 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { Route, Routes } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+import './App.css';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import AboutPage from './pages/AboutPage';
+
+function App() {
+    return (
+        <div className='App'>
+            <Header />
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='contact' element={<ContactPage />} />
+                <Route path='about' element={<AboutPage />} />
+                <Route path='directory' element={<CampsitesDirectoryPage />} />
+                <Route
+                    path='directory/:campsiteId'
+                    element={<CampsiteDetailPage />}
+                />
+            </Routes>
+            <Footer />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/features/partners/PartnersList.js
@@ -0,0 +1,18 @@
+import { Col } from 'reactstrap';
+import Partner from './Partner';
+import { selectAllPartners } from './partnersSlice';
+
+export default function PartnersList() {
+    const partners = selectAllPartners();
+    return (
+        <Col className='mt-4'>
+            {partners.map((partner) => {
+                return (
+                    <div className='d-flex mb-5' key={partner.id}>
+                        <Partner partner={partner} />
+                    </div>
+                )
+            })}
+        </Col>
+    )
+}
